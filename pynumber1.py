238893289cfda70bae63874157d6a47f7abf118b
ankit3466 <48724966+ankit3466@users.noreply.github.com>
2019-06-25 20:21:18 +0530

    Update pynumber1.py

--- pynumber1.py
+++ pynumber1.py
@@ -1,11 +1,15 @@
+# importing numpy library
 import numpy as np
 
+# taking inputs from user
 r=int(input("Enter no of row"))
 c=int(input("Enter no of cplumn"))
 
+# creating array of user's dimensions
 z=np.random.random((r,c))
 #print(z)
 
+# writing the content in a file using file handling
 f=open('numpy.txt','w')
 f.write(str(z))
 f.close()
